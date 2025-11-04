147def714f058b501e4c33f0e81861fd39f7d868
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-04 15:45:35 +0100

    Refactor + gestion des chiffres corrects masi au mauvaise endroits

--- src/tp3CodeSecret/Tp3CodeSecret.java
+++ src/tp3CodeSecret/Tp3CodeSecret.java
@@ -10,13 +10,12 @@ public class Tp3CodeSecret {
     	//Affectation de variables
         Random random = new Random(); 
         Scanner scan = new Scanner(System.in);
-        boolean isContinue = true;
+        Boolean codeFound = false ;
         
-      
         String code = generateCode(random);
         System.out.println("CODE SECRET : " + code);
 
-        while (isContinue) {
+        while (codeFound == false) {
         	
             String userCode = userInputCode(scan);
 
@@ -29,32 +28,70 @@ public class Tp3CodeSecret {
             boolean[] userUsed = new boolean[userCodeArray.length];
             
             
-            // Boucle pour trouver les chiffres corrects et bien placés
-            for (int i_index = 0; i_index < codeSecretArray.length; i_index++) {
-            	if (codeSecretArray[i_index] == userCodeArray[i_index]) {
-
-            		inCorrectPostion ++;
-            	}	
-            }
+            inCorrectPostion = correctWithGoodPosition(codeSecretArray, userCodeArray, inCorrectPostion, codeUsed,userUsed);
+            inWrongPosition = correctButNotCorrectPosition(codeSecretArray, userCodeArray, inWrongPosition, codeUsed,userUsed);
             
-            // Boucle pour trouver les chiffres corrects mais mal placés
-            for (int i_index = 0; i_index < userCodeArray.length; i_index++) {
-                if (userUsed[i_index]) {
-                	continue; 
-                }
-                for (int j_index = 0; j_index < codeSecretArray.length; j_index++) {
-                    if (!codeUsed[j_index] && userCodeArray[i_index] == codeSecretArray[j_index]) {
-                    	inWrongPosition++;
-                        codeUsed[j_index] = true;
-                        break;
-                    }
-                }
-            }
-
             System.out.println(inCorrectPostion + " chiffre bien placé ; " + inWrongPosition + " chiffre mal placé");
+            
+            if(inCorrectPostion == 4) {
+            	System.out.print("Vous avez trouvé le code");
+            	codeFound = true ; 
+            	break;
+            }
         }
         scan.close();
     }
+
+
+	private static int correctWithGoodPosition(char[] secretCodeArray, char[] userCodeArray, int inCorrectPostion, boolean[] codeUsed, boolean[] userUsed) {
+		/**
+		 * Méthode permettant de savoir si un chiffre est au bonne endroit
+		 * param secretCodeArray : liste des characters du code à trouver
+		 * param userCodeArray : liste des characters du code saisie par l'utilisateur
+		 * param inCorrectPostion : nombre de characters à la bonne position
+		 * param codeUsed : permet de savoir si une valeur du code générée à été utilisée
+		 * param userUsed : permet de savoir si une valeur du code saisie par l'utilisateur à été utilisée
+		 * return inCorrectPostion : nombre de valeurs bien placés
+		 */
+		
+		for (int i_index = 0; i_index < secretCodeArray.length; i_index++) {
+			if (secretCodeArray[i_index] == userCodeArray[i_index]) {
+
+				inCorrectPostion ++;
+				  codeUsed[i_index] = true; 
+			      userUsed[i_index] = true; 
+			}	
+		}
+		return inCorrectPostion;
+	}
+
+
+	private static int correctButNotCorrectPosition(char[] secretCodeArray, char[] userCodeArray, int inWrongPosition, boolean[] codeUsed, boolean[] userUsed) {
+		/**
+		 * Méthode permettant de savoir si un chiffre est correct mais au mauvaise endroit
+		 * param secretCodeArray : liste des characters du code à trouver
+		 * param userCodeArray : liste des characters du code saisie par l'utilisateur
+		 * param inCorrectPostion : nombre de characters à la bonne position
+		 * param codeUsed : permet de savoir si une valeur du code générée à été utilisée
+		 * param userUsed : permet de savoir si une valeur du code saisie par l'utilisateur à été utilisée
+		 * return inCorrectPostion : nombre de valeurs bien placés
+		 */
+		
+		// Boucle pour trouver les chiffres corrects mais mal placés
+		for (int i_index = 0; i_index < userCodeArray.length; i_index++) {
+		    if (userUsed[i_index]) {
+		    	continue; 
+		    }
+		    for (int j_index = 0; j_index < secretCodeArray.length; j_index++) {
+		        if (!codeUsed[j_index] && userCodeArray[i_index] == secretCodeArray[j_index]) {
+		        	inWrongPosition++;
+		            codeUsed[j_index] = true;
+		            break;
+		        }
+		    }
+		}
+		return inWrongPosition;
+	}
 
 
 	private static String userInputCode(Scanner scan) {
@@ -73,18 +110,4 @@ public class Tp3CodeSecret {
         }
         return code;
     }
-
-	private static Boolean isContinueGame(Boolean isContinue, Scanner scan) {
-		
-		 System.out.print("Voulez-vous réessayer ? (taper O ou N) : ");
-		 System.out.println();
-		 String choice = scan.next();
-		
-		if (choice.equalsIgnoreCase("O")) {
-     
-		} else {
-			  isContinue = false;
-		}
-		return isContinue;
-	}
 }
