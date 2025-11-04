0215b2f4b09903cffb602c9096762624f28627e5
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-04 15:11:33 +0100

    Suppression de la méthode isContinueGame

--- src/tp3CodeSecret/Tp3CodeSecret.java
+++ src/tp3CodeSecret/Tp3CodeSecret.java
@@ -7,39 +7,62 @@ public class Tp3CodeSecret {
 
     public static void main(String[] args) {
 
+    	//Affectation de variables
         Random random = new Random(); 
         Scanner scan = new Scanner(System.in);
         boolean isContinue = true;
-        int inCorrectPostion = 0;
-
-
+        
+      
         String code = generateCode(random);
         System.out.println("CODE SECRET : " + code);
 
         while (isContinue) {
-            System.out.print("Veuillez entrer un code : ");
-            String userCode = scan.next();
-            System.out.println("CODE UTILISATEUR : " + userCode);
+        	
+            String userCode = userInputCode(scan);
 
-
+            // Affectation de variables
             char[] codeSecretArray = code.toCharArray();
             char[] userCodeArray = userCode.toCharArray();
-
+            int inCorrectPostion = 0;
+            int inWrongPosition = 0;
+            boolean[] codeUsed = new boolean[codeSecretArray.length];
+            boolean[] userUsed = new boolean[userCodeArray.length];
+            
+            
+            // Boucle pour trouver les chiffres corrects et bien placés
             for (int i_index = 0; i_index < codeSecretArray.length; i_index++) {
             	if (codeSecretArray[i_index] == userCodeArray[i_index]) {
 
             		inCorrectPostion ++;
-            		
-            	}
-            	
+            	}	
+            }
+            
+            // Boucle pour trouver les chiffres corrects mais mal placés
+            for (int i_index = 0; i_index < userCodeArray.length; i_index++) {
+                if (userUsed[i_index]) {
+                	continue; 
+                }
+                for (int j_index = 0; j_index < codeSecretArray.length; j_index++) {
+                    if (!codeUsed[j_index] && userCodeArray[i_index] == codeSecretArray[j_index]) {
+                    	inWrongPosition++;
+                        codeUsed[j_index] = true;
+                        break;
+                    }
+                }
             }
 
-            System.out.println(inCorrectPostion + " chiffre bien placé ; 1 chiffre mal placé");
-            isContinue = isContinueGame(isContinue,scan);
+            System.out.println(inCorrectPostion + " chiffre bien placé ; " + inWrongPosition + " chiffre mal placé");
         }
-
         scan.close();
     }
+
+
+	private static String userInputCode(Scanner scan) {
+		System.out.print("Veuillez entrer un code : ");
+		String userCode = scan.next();
+		System.out.println("CODE UTILISATEUR : " + userCode);
+		return userCode;
+	}
 
 
     private static String generateCode(Random random) {
